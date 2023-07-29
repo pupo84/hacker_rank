85b5bd4e88f6c9ef9342f813658c4052a59adfc0
Matheus Pupo <matheus.pupo84@gmail.com>
2023-07-29 11:19:15 -0300

    Add string exercises

---
+++ python/strings/split_and_join.py
@@ -0,0 +1,6 @@
+# https://www.hackerrank.com/challenges/python-string-split-and-join/problem
+def split_and_join(line):
+    return "-".join(line.split(" "))
+
+
+print(split_and_join("this is a string"))
---
+++ python/strings/swap_case.py
@@ -0,0 +1,8 @@
+# https://www.hackerrank.com/challenges/swap-case/problem
+
+
+def swap_case(string):
+    return string.swapcase()
+
+
+print(swap_case("Www.HackerRank.com → wWW.hACKERrANK.COM"))
